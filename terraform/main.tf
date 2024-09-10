ebd7fef41cbb3dfbb8b10e4e63a9debc5dc42244
Poornima <pd.poornima@gmail.com>
2024-09-10 23:50:44 +0530

    added terraform providers in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.0.1"  # Adjust this version based on your requirements
+    }
+  }
+
+  required_version = ">= 1.9.5"  # Ensure this matches the version in Cloud Build
+}
 provider "google" {
   credentials = file("C:/Users/Poornima/Documents/service-account-key.json")
   project     = var.project_id
